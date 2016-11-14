84820b9a1904afc548b5cd15e3a901c5fc95863b
Austin Matthews <boston.austin@gmail.com>
2016-11-14 17:16:30 -0500

    add logrotate to mongo DB host

---
+++ database/mongoDB_logrotate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mongodb_log="/etc/logrotate.d/mongodb"
+
+# ensure logrotate is installed on server
+type logrotate >/dev/null 2>&1 || apt-get -y -qq install logrotate
+
+# check to see if mongoDB logrotate config file exists and create if missing
+if [ ! -f "${mongodb_log}" ]; then
+  cat > /etc/logrotate.d/mongodb <<EOF
+/var/log/mongodb/*.log {
+daily
+rotate 7
+compress
+dateext
+missingok
+notifempty
+sharedscripts
+copytruncate
+}
+EOF
+  echo "restarting the logrotate service to apply changes ... "
+  service logrotate restart
+else
+  echo "[ERROR] the file ${mongodb_log} exists already, please remove manually and re-run ${basename}"
+  exit "2"
+fi
+
+echo ""
+echo "[PASSED] a MongoDB logrotate entry has been added successfully for this server"
+exit "0"
